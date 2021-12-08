ab8c2a33d7b7b28938e104abf0559d72fb09284a
Flavio Moreira <34199219+fdvmoreira@users.noreply.github.com>
2021-12-08 09:52:07 +0000

    Write the major steps to calculate the ip address

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 
+// ********************** TODOs **************************
+//
+// [] get ip address
+// [] split the ip address from subnetmask
+// [] validate the ip address
+// [] construct the subnetmask
+// [] validate the subnetmask
+// [] AND the mask to find network and host portions
+// [] count the subnet and host bits
+// [] get the network and the broadcast address of each subnet
+// [] print the results
+// 
+// 
+
+
 const form = document.querySelector("form");
 const ipAddressTextField = document.getElementById('ip-address-tf');
 form.onsubmit = () => {
