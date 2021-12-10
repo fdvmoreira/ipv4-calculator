f5ca6b11cb8ab3e5b98885328709c848b1484f3e
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-12-10 11:20:56 +0000

    Get the IP address and subnetmask and validate IP

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
 // ********************** TODOs **************************
 //
-// [] get ip address
-// [] split the ip address from subnetmask
-// [] validate the ip address
+// [*] get ip address
+// [*] split the ip subnetmask from address
+// [*] validate the ip address
 // [] construct the subnetmask
 // [] validate the subnetmask
 // [] AND the mask to find network and host portions
@@ -17,5 +17,36 @@
 const form = document.querySelector("form");
 const ipAddressTextField = document.getElementById('ip-address-tf');
 form.onsubmit = () => {
-    alert(ipAddressTextField.value);
+    // alert(ipAddressTextField.value);
+    let input = ipAddressTextField.value;
+
+    if (!input.trim().length > 0) return;//show error and ruturn
+    let ipAddress = input.split('.');
+    if (ipAddress.length !== 4) return;// invalid ip address
+
+    // get the subnetmask from ipAddress' last octect and convert it to into integer
+    let subnetMask = parseInt(ipAddress[3].split('/')[1]);
+
+    // convert to integer and validate ip IP Address
+    ipAddress[3] = ipAddress[3].split('/')[0];
+    ipAddress.forEach((value, index, array) => {
+        array[index] = parseInt(value);
+    });
+
+    if (!(ipAddress.every(element => isInRange(element, 0, 255)))) {
+        // log error
+        return;
+    }
+
+}
+
+/**
+ * check the if a number is in range
+ * @param {Integer} number - current number 
+ * @param {Integer} min - lowest value
+ * @param {Integer} max - highest value
+ * @returns true if the is between min and max
+ */
+const isInRange = (number, min, max) => {
+    return (number >= min && number <= max);
 }
