37879ffcf07c39c7336cc5dc58641996a5ca92f6
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-12-25 14:36:11 +0000

    add E class to the list of classes

--- js/IPClass.js
+++ js/IPClass.js
@@ -1,7 +1,9 @@
-class IPClass {
+export default class IPClass {
     static A = Symbol("A");
     static B = Symbol("B");
     static C = Symbol("C");
     static D = Symbol("D");
+    static E = Symbol("E");
+
 }
 
