577ddb4293d84b394cdec78f81d1f1a7a0fce0bb
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-12-25 15:28:31 +0000

    add getIp4Class function to test app test suite

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,24 @@
 // let app = require("../js/app.js");
 import { isInRange, getIpv4Class } from '../js/app';
+import IPClass from '../js/IPClass';
 
-test('189 is in range 0-255', () => {
-    expect(isInRange(189, 0, 255)).not.toBeFalsy();
+describe("isInRange()", () => {
+    it('expects 189 to be in range 0-255', () => {
+        expect(isInRange(189, 0, 255)).toBeTruthy();
+    });
+});
+
+describe("getIp4Class", () => {
+    it('expects 256 to be invalid first octect', () => {
+        expect(getIpv4Class(256)).toMatch("Invalid first Octect.");
+    });
+    it('expects -1 to be invalid first octect', () => {
+        expect(getIpv4Class(-1)).toMatch("Invalid first Octect.");
+    });
+    it('expects function not to return undefined', () => {
+        expect(getIpv4Class(0)).toBeDefined();
+    });
+    it("expects 10 to be class A ", () => {
+        expect(String(getIpv4Class(10))).toMatch(IPClass.A.toString());
+    });
 });
