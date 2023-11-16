9d1a7b49b197d3af384f52a8011b5eff6cec6010
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-11-16 20:46:05 +0000

    Clean the content and of files and delete redundant ones for restructure and documentation

--- js/IPClass.js
+++
@@ -1,9 +0,0 @@
-export default class IPClass {
-    static A = Symbol("A");
-    static B = Symbol("B");
-    static C = Symbol("C");
-    static D = Symbol("D");
-    static E = Symbol("E");
-
-}
-
