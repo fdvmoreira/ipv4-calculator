e6318645f45ccb0ecf423ff8fdd47601ceae0b64
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-11-18 16:38:28 +0000

    Add test for validateIpOctets function

--- test/app.test.js
+++ test/app.test.js
@@ -1,2 +1,29 @@
+
 /** @jest-environment jsdom */
 
+import { describe, expect, test } from "@jest/globals";
+import { validateIpOctects } from "../js/app";
+
+/**
+ * @group App
+ */
+describe("validateIpOctects", () => {
+  /**
+   * @test {App}
+   */
+  test("should validate validate VALID Ip address", () => {
+    expect(validateIpOctects([0, 0, 0, 0])).toBeTruthy();
+    expect(validateIpOctects([1, 2, 3, 4])).toBeTruthy();
+    expect(validateIpOctects([255, 255, 255, 255])).toBeTruthy();
+  });
+
+  /**
+   * @test {App}
+   */
+  test("should Invalidate Ip addresses out of range", () => {
+    expect(validateIpOctects([0, 0, 0, 256])).toBeFalsy();
+    expect(validateIpOctects([-1, 0, 0, 25])).toBeFalsy();
+    expect(validateIpOctects([-2, 255, 255, 255])).toBeFalsy();
+    expect(validateIpOctects([127, 0, 0, 0])).toBeFalsy();
+  });
+});
