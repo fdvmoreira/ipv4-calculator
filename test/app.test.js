5231120b23a01a03b93d8e8abada6caae236c8d6
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-11-19 18:58:48 +0000

    Add test for validating slash mask

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@
 /** @jest-environment jsdom */
 
 import { describe, expect, test } from "@jest/globals";
-import { validateIpOctects } from "../js/app";
+import { IpClasses, validateIpOctects, validateSlashMask } from "../js/app";
 
 /**
  * @group App
@@ -25,5 +25,26 @@ describe("validateIpOctects", () => {
     expect(validateIpOctects([-1, 0, 0, 25])).toBeFalsy();
     expect(validateIpOctects([-2, 255, 255, 255])).toBeFalsy();
     expect(validateIpOctects([127, 0, 0, 0])).toBeFalsy();
+  });
+});
+
+/**
+ * @group App
+ */
+describe("validateSlashMask", () => {
+  /**
+   * @test {App}
+   */
+  test("should validate slash mask", () => {
+    expect(validateSlashMask(IpClasses.A, 24)).toBeTruthy();
+    expect(validateSlashMask(IpClasses.A, 7)).toBeFalsy();
+    expect(validateSlashMask(IpClasses.B, 16)).toBeTruthy();
+    expect(validateSlashMask(IpClasses.B, 24)).toBeTruthy();
+    expect(validateSlashMask(IpClasses.B, 15)).toBeFalsy();
+    expect(validateSlashMask(IpClasses.C, 23)).toBeFalsy();
+    expect(validateSlashMask(IpClasses.A, 26)).toBeTruthy();
+    expect(validateSlashMask(IpClasses.A, 31)).toBeFalsy();
+    expect(validateSlashMask(IpClasses.D, 8)).toBeFalsy();
+    expect(validateSlashMask(IpClasses.E, 8)).toBeFalsy();
   });
 });
